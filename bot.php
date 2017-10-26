38e70f47f624ed9211a4c5b1e84dead20f324d10
TheAdnan <adnankicin92@gmail.com>
2017-10-26 15:56:05 +0200

    logger

--- bot.php
+++ bot.php
@@ -1,11 +1,17 @@
 <?php 
+error_reporting(-1);
 
 require "vendor/autoload.php";
 include 'credentials.php';
 use Abraham\TwitterOAuth\TwitterOAuth;
+use Analog\Analog;
 
 ini_set('memory_limit', '2048M');
- 
+
+
+$log_file = 'bot.log';
+Analog::handler (Analog\Handler\File::init ($log_file));
+
 
 function search(array $query){
 	global $connection;
@@ -16,19 +22,21 @@ $connection = new TwitterOAuth($consumer_key, $consumer_secret, $access_key, $ac
 
 $query = array(
   'q' => 'bosanac OR bosanci OR bosancu OR bosanca OR bosancima',
-  'count' => '10',
+  'count' => '20',
   'result_type' => 'recent'
 );
   
 $results = search($query);
   
 foreach ($results->statuses as $result) {
+   $text = $result->text;
    $user = $result->user->screen_name;
    $newtweet = "bošnjak*";
    $replyuser = "@" . $user ;
    $reply = "$replyuser  $newtweet";
    echo $reply  . "\n";
    $tweetID = $result->id_str;
+   Analog::log('{$reply} to Tweet: $text \n');
    //Reply to tweet
    $response = $connection->post('statuses/update' , array('status' => $reply, 'in_reply_to_status_id' => $result->id_str));
 }
