072985f88f6c47c32949a253526d0aebbaef7a3c
TheAdnan <adnankicin92@gmail.com>
2017-10-26 10:59:12 +0200

    credentials example

---
+++ credentials.php
@@ -0,0 +1,12 @@
+<?php
+
+//Credentials for this bot
+//these are obviously not the actual credentials
+//I would never use meho123
+//mehmed123 maybe, but meho123 never
+
+	$consumer_key = "meho123";
+	$consumer_secret = "meho123";
+	$access_key = "meho123";
+	$access_secret = "meho123";
+?>
